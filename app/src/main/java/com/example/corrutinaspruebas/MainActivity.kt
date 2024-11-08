3306d5ee88fd0592aaa2b6babedd15f9b329cdd7
alvarikola <alvaroe484@gmail.com>
2024-11-08 11:57:39 +0000

    realizada la interfaz en un composable

--- app/src/main/java/com/example/corrutinaspruebas/MainActivity.kt
+++ app/src/main/java/com/example/corrutinaspruebas/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -30,33 +31,35 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
-            val viaje = remember { mutableStateOf("Esperando para subir al cohete...") }
-            val coroutineScope = rememberCoroutineScope()
-
             // Interfaz
-            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(innerPadding)
-                        .padding(16.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    Text(text = "Estado del viaje: ${viaje.value}")
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Button(onClick = {
-                        // Lanzamos la corutina dentro de un composable
-                        // y actualizamos el estado de viaje
-                        coroutineScope.launch {
-                            viaje.value = hacerViaje()
-                        }
-                    }) {
-                        Text(text = "Iniciar viaje")
-                    }
-                }
+            Surface {
+                Interfaz()
             }
+        }
+    }
+}
+
+
+@Composable
+fun Interfaz() {
+    val viaje = remember { mutableStateOf("Esperando para subir al cohete...") }
+    val coroutineScope = rememberCoroutineScope()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Estado del viaje: ${viaje.value}")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            // Lanzamos la corutina dentro de un composable y actualizamos el estado de viaje
+            coroutineScope.launch {
+                viaje.value = hacerViaje()
+            }
+        }) {
+            Text(text = "Iniciar viaje")
         }
     }
 }
